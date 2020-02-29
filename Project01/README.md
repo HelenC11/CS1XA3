93eba7f1fb47e7e97c9dcbc636f0db6a08c5199d
Helen <chenh214@mcmaster.ca>
2020-02-29 04:48:17 +0000

    Submitting Project 01 Part 2

--- Project01/README.md
+++ Project01/README.md
@@ -6,7 +6,12 @@ chmod +x CS1XA3/Project01/project_analyze.sh
 With possible arguments
 - arg1: LatestMerge
 - arg2: FileTypeCount
-- arg3:FIXME
+- arg3: FIXME
+- arg4: TryCommit
+- arg5: Switch 
+- arg6: Backup
+- arg7: NewDir
+
 ## Feature 01 Checkout Latest Merge
 Description: this feature finds and will checkout the latest merge commit
 
@@ -19,7 +24,7 @@ Description: this feature returns the number of files in a repo of a certain typ
 
 Execution: execute this feature by inputting FileTypeCount after the action prompt and then inputting an extension type
 
-Reference: some code was taken from [here](https://askubuntu.com/questions/454564/count-total-number-of-files-in-particular-d)
+Reference: some code was taken from [here](https://askubuntu.com/questions/454564/count-total-number-of-files-in-particular-directory-with-specific-extension)
 
 ## Feature 03 FIXME Log
 
@@ -30,24 +35,33 @@ Execution:Input FIXME after the action prompt
 Reference:[here](https://cmdlinetips.com/2011/08/how-to-count-the-number-of-lines-words-and-characters-in-a-text-file-from-te$)
 
 ## Feature 04 Backup and Delete / Restore:
-Description: 
 
-Execution: 
+Description: This feature stores all files ending with .tmp in a directory and will restore them to their orignal location if prompted
 
-Reference:
+Execution: Input Backup, then Backup to backup the files. Rerun and enter Restore, to Restore files
 
-## Feature 04 Backup and Delete / Restore:
+Reference:[here](https://www.ostechnix.com/find-copy-certain-type-files-one-directory-another-linux/)
 
-Description:
+## Feature 04 Switch to Executable:
 
-Execution:
+Description: This feature will add executable perms to anyone with write perms on  .sh files
 
-Reference:
+Execution:Input Switch and then Change to change perms and Restore to restore perms
+
+Reference:[here](https://askubuntu.com/questions/229589/how-to-make-a-file-e-g-a-sh-script-executable-so-it-can-be-run-from-a-termi)
 
 ## Custom Features
 
-Possible custom feature 1:A feature that adds, commits and pushes a new directory with a README.md file
+Custom feature 1:A feature that adds, commits a new directory with a README.md file 
 
-Possible custom feature 2:A feature that validates that a README.md file matches a certain pattern in order to commit
+Execution: input NewDir and a name
 
-Custom Feature02:
+Reference: [here](https://github.com/jessica-dl/COMPSCI-1XA3/blob/master/Assign1/ProjectAnalyze.sh)
+
+Custom feature 2:A feature that validates that a README.md file matches a certain pattern(must have a certain phrase or word) in order to commit
+
+Execution: input TryCommit and choose to add the pattern or not
+
+Reference: [here](https://git-scm.com/book/en/v2/Customizing-Git-Git-Hooks)
+
+tryMe
--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,10 +1,144 @@
 #!/bin/bash
-echo "Hello, what action would you like to perform today? Please enter LatestMerge, FileTypeCount, or FIXME"
-echo "LatestMerge: will find and checkout the latest merge"
-echo "FileTypeCount: Will give you the number of files of a certain type"
-echo "FIXME: Will give you a log of all the files with #FIXME on the last line"
-read action
-if [ "$action" = "LatestMerge" ]  ; then
+
+echo "Hello what action would you like to do? Switch, Backup,TryCommit, NewDir,LatestMerge, FileTypeCount, or FindTag"
+
+
+#sorry for the long phrases as args I had troubles telling numbers apart for testing
+read prompt
+
+if [ "$prompt" = "Backup" ] ; then
+	
+	# Backup delete and restore
+	echo "Hello would you like to Backup or Restore? Please type the action you would like to do (Backup/Restore)"
+	read action
+
+	if [ "$action" = "Backup" ] ; then
+        	if [ -d "backup" ] ; then
+                	rm -r backup
+                	mkdir backup
+        	else
+                	mkdir backup
+        	fi
+
+
+		touch ./backup/restore.log
+		#find  . -type f -name "*.tmp" > ./backup/restore.log
+        	find -iname '*.tmp' -exec cp {} ./backup/ \; -exec rm {} \; -exec echo "$PWD" >> ./backup/restore.log \;
+
+	elif [ "$action" = "Restore" ]
+	then
+        	echo "restoring"
+		if [ -f "./backup/restore.log" ] ; then
+			echo "Returning files"
+	
+          		while read PERMS  FILE
+          		do
+             			fullfile=$FILE
+             			filename="${FILE##*/}"
+             			cp ./backup/$filename $fullfile
+          		done <./backup/restore.log
+		else
+			echo "Error: log does not exist, nothing to restore"
+		fi
+
+	else
+        echo "That is not a valid command. Please rerun the script and try again"
+	fi
+
+#switch to executable
+elif [ "$prompt" = "Switch" ] 
+then 
+
+	echo Hello, what action would you like to perform today? Please enter Change, Restore
+
+	read action
+
+	
+
+
+
+	if [ "$action" = "Change" ]
+	then
+		for file in *.sh; do
+			stat -c '%a %n' *.sh > permission.log
+		done
+		cat permission.log  
+ 
+		for file in *.sh; do
+   
+			echo $file
+   			if
+				test $(find $file -perm -u+w |wc -l) = 1
+			then
+				chmod u+x $file
+			fi
+
+			if
+				test $(find $file -perm -g+w |wc -l) = 1
+			then
+				chmod g+x $file
+			fi
+
+			if
+				test $(find $file -perm -o+w |wc -l) = 1
+			then
+				chmod o+x $file
+			fi
+   		done
+  
+	elif  [ "$action" = "Restore"  ]
+	then	
+		echo restore
+		while read PERMS  FILE
+		do
+			chmod  "$PERMS"  "$FILE"
+
+        		done <permission.log
+
+   	else
+
+        	echo  That is not an legal action, please rerun the script and try again
+
+   	fi
+
+#custom2
+elif [ "$prompt" = "TryCommit" ]
+then
+	echo " Do you want to add or remove commit pattern check (A/R)"
+	read reply
+	if [  "$reply" = "A" ] ; then
+	
+		mv pre-commit ../.git/hooks
+		chmod +x ../.git/hooks/pre-commit
+	elif [ "$reply" = "R" ] 
+	then	
+		cp ../.git/hooks/pre-commit ./Project01
+		rm ../.git/hooks/pre-commit
+	fi
+#custom1
+elif [ "$prompt" = "NewDir" ]
+then
+	echo "What would you like to name this new Directory? " 
+	read name
+        if [ ! -d $name ] 
+        then
+        	mkdir $name
+		touch $name/README.md 
+		echo "# This is README for $name" >> $name/README.md
+
+		read -p "Please enter text for the README: " txt
+		echo -e "$txt\n" >> $name/README.md
+
+		git add $name/README.md
+    	    	git add $name
+    	    	git commit "$name" -m "Created $name"
+	else
+            echo -e "$A directory of that name already exists."
+        fi
+
+#LatestMerge
+elif [ "$prompt" = "LatestMerge" ]
+then
         p=$(   git show -s  --format=%h :/merge )
 
         if [ "$p" = "" ]
@@ -15,31 +149,32 @@ if [ "$action" = "LatestMerge" ]  ; then
                 echo $p
                 git checkout $p
         fi
-
-elif [ "$action" = "FileTypeCount" ] ; then
+#fileTypeCount
+elif [ "$prompt" = "FileTypeCount" ]
+then
         echo "Please input an extension (txt, pdf, etc)"
         read extension
         echo "Number of files of this type"
         #ls -lR ./*.$extension | wc -l
         cd ..
-        find ./** -name *."$extension" | wc -l
+        find ./** -name *.$extension | wc -l
+#findtag
+elif [ "$prompt" = "FindTag" ]
+then
 
-elif [ "$action" = "FIXME" ] ; then
-	rm fixme.log	
-	for file in *; do 
-
- 		if [ -f "$file" ]; then 
-			count=$(tail -1 $file | grep '#FIXME' | wc -l)
-       			if [ $count -gt 0 ]; then
-
-        			echo $file >> 'fixme.log'
-       			fi
-    		fi 
-
-	done
+        if [ -f "fixme.log" ] ; then
+                rm fixme.log
+                touch fixme.log
+        else
+                touch fixme.log
+        fi
+        #tail -n 1 ./** | grep "#FIXME
+        tail -f ./** | grep "#FIXME"
 
 
-else
+        #grep -r "#FIXME" nix* | tail -Z
+
+else 
 	echo "That is not a legal action, please rerun the script and try again"
 fi
-
+ 
