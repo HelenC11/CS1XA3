774d084b48550f9d2befbfec80e63aed3af6539b
HelenC11 <59624330+HelenC11@users.noreply.github.com>
2020-03-18 20:05:11 -0400

    finishing project2

--- Project02/js/test.js
+++ Project02/js/test.js
@@ -1,10 +1,20 @@
 var purple = document.getElementById(".desc .e-heading"); 
 
 function recolor()
-{
-    purple.setAttribute("id",".desc .e-heading2")
+{  
+    var a = document.getElementsByClassName("e-heading");
+    
+    for (var i=0; i<a.length; i++) a[i].style.color = '#732673';
+
+    document.getElementById("test").style.color='#732673';
+    document.body.style.backgroundImage = "url('./img/violet.jpg')";
+    
 }
 function recolor2()
 {
-    purple.setAttribute("id",".desc .e-heading")
+    var a = document.getElementsByClassName("e-heading");
+    for (var i=0; i<a.length; i++) a[i].style.color = '#e74c3c';
+    document.getElementById("test").style.color='#e74c3c';
+        
+    document.body.style.backgroundImage = "url(./img/bg.jpg)";
 }
